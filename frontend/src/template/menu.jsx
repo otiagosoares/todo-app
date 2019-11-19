914b2ac7cebe9464033ebe97acf0895f327b46f6
Tiago Soares <tiago-s@outlook.com>
2019-11-19 01:13:33 -0300

    grid css bootstrap

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default props => (
 
-		<nav className="navbar navbar-inverse bg-inverse">
-			<div className="container">
-				<div className="navbar-header">
-					<a className="navbar-brand" href='#'>
-						<i className='fa fa-calendar-check-o'></i> TodoApp
-					</a>
-				</div>
-				<div id="navbar" className="navbar-collapse collabse">
-					<ul className="nav navbar-nav">
-						<li><a href="#/todos">Tasks</a></li>
-						<li><a href="#/about">Sobre</a></li>
-					</ul>
-				</div>
+	<nav className="navbar navbar-inverse bg-inverse">
+		<div className="container">
+			<div className="navbar-header">
+				<a className="navbar-brand" href='#'>
+					<i className='fa fa-calendar-check-o'></i> TodoApp
+				</a>
 			</div>
-		</nav>
+			<div id="navbar" className="navbar-collapse collabse">
+				<ul className="nav navbar-nav">
+					<li><a href="#/todos">Tasks</a></li>
+					<li><a href="#/about">Sobre</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
 )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
 
-	<div>
-		Fromulario
+	<div role="form" className="todoForm">
+		<Grid cols='12 9 10'>
+			<input type="text" id="description" className="form-control" placeholder="Adicione uma tarefa"/>
+		</Grid>
+		<Grid cols='12 3 2'>
+			<IconButton style='primary' icon='plus' />
+		</Grid>
 	</div>
 
 )
