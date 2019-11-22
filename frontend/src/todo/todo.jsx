1ad2908c7a4e0a066c7bfef38d489fa67649391b
Tiago Soares <tiago-s@outlook.com>
2019-11-22 00:53:48 -0300

    set state and onchange input

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,6 +4,27 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+	constructor(props){
+		super(props)
+		//seta estado inicial
+		this.state = { description: '', list: [] }
+
+		this.handleAdd = this.handleAdd.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleAdd(){
+
+		console.log(this.state.description)
+
+	}
+
+	handleChange(e){
+
+		this.setState({...this.state, description: e.target.value})
+
+	}
 	
 	render(){
 
@@ -11,7 +32,11 @@ export default class Todo extends Component {
 
 			<div>
 				<PageHeader name="Tarefas"small="Cadastro"></PageHeader>
-				<TodoForm />
+				<TodoForm 	
+					handleAdd={this.handleAdd} 
+					handleChange={this.handleChange}
+					description={this.state.description}
+					/>
 				<TodoList />
 			</div>
 
